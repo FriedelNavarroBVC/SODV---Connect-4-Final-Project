799b07e15943f8c8ace7e3a1e29ac3080264d390
Friedel <Friedel@YOGA-C940>
2023-06-12 11:08:38 -0600

    Add project files.

---
+++ SODV - Connect 4 Final Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SODV___Connect_4_Final_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
