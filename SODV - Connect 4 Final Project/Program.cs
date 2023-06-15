a8caca5e503cb4a04beacc36e0d01e356732a4ab
Friedel <f.navarro417@mybvc.ca>
2023-06-15 13:03:31 -0600

    Phase 1: Basic Game Structure
    1. Create the main part of the SODV___Connect_4_Final_Project program, which includes the Program class and the Main method.
    2. Describe the Game class and include the required information and actions.
    3. Describe the Board class and include the required information and actions.
    4. Describe the Player class and include the required information and actions.
    5. Make the Game class set up the basic game process by setting up the game board, players, and current player in the constructor.
    6. Make the Game class have a Start method that shows the game menu and handles the selection of the game mode.

--- SODV - Connect 4 Final Project/Program.cs
+++ SODV - Connect 4 Final Project/Program.cs
@@ -2,44 +2,81 @@
 
 namespace SODV___Connect_4_Final_Project
 {
-    internal class Program
+    class Program
     {
-        static void (Main(string[] args)
-            {
+        static void Main(string[] args)
+        {
             Game game = new Game();
-        game.Start();
-
+            game.Start();
         }
     }
 
-class Game
-
-{
-    private Board board;
-    private Player player1;
-    private Player player2;
-    private Player currentPlayer;
-    private bool isGamefinished;
-
-    public Game()
+    class Game
     {
-        board = new Board();
-        player1 = new Player('X', "Player 1");
-        Player2 = new Player('O', "Computer");
-        currentPlayerPlayer = player1;
-        isGamefinished = false;
+        private Board board; // Represents the game board
+        private Player player1; // Represents player 1
+        private Player player2; // Represents player 2 or the computer
+        private Player currentPlayer; // Represents the current player
+        private bool isGameFinished; // Indicates if the game is finished
+
+        public Game()
+        {
+            board = new Board();
+            player1 = new Player('X', "Player 1");
+            player2 = new Player('O', "Player 2");
+            currentPlayer = player1;
+            isGameFinished = false;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Connect Four!");
+            Console.WriteLine("Please select a mode:");
+            Console.WriteLine("1. 2-Player Mode");
+            Console.WriteLine("2. 1-Player Mode (Player vs Computer)");
+            Console.WriteLine("3. Exit");
+
+            char mode = Console.ReadKey().KeyChar; // Read the selected mode from the user
+            Console.WriteLine();
+
+            switch (mode)
+            {
+                case '1':   
+                    TwoPlayerMode(); // Start the 2-player mode
+                    break;
+                case '2':
+                    SinglePlayerMode(); // Start the 1-player mode
+                    break;
+                case '3':
+                    Environment.Exit(0); // Exit the application
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.\n");
+                    Start(); // Prompt the user to select a valid mode
+                    break;
+            }
+        }
     }
 
-}
-
-class Player
-{
-    public char Symbol { get; }
-    public string Name { get; }
-
-    public Player (char symbol, string name)
+    class Board
     {
-        Symbol= symbol;
-        Name= name;
+        private char[,] grid; // Represents the game board grid
+
+        public Board()
+        {
+            grid = new char[6, 7]; // Initialize the game board grid with a size of 6x7
+        }
+    }
+
+    class Player
+    {
+        public char Symbol { get; } // Represents the player's symbol (X or O)
+        public string Name { get; } // Represents the player's name
+
+        public Player(char symbol, string name)
+        {
+            Symbol = symbol;
+            Name = name;
+        }
     }
 }
