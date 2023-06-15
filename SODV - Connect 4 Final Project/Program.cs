03cf60fc06ee0f2a41eddf58e15e6864203fa135
Friedel <f.navarro417@mybvc.ca>
2023-06-15 12:11:28 -0600

    Phase 1: Game Initialization Create a Game class that represents the Connect Four game. Create a Game class that represents the Connect Four game. Initialize the game by creating a Board, two Player objects, and setting the current player as player1. Set the isGameFinished flag to false.

--- SODV - Connect 4 Final Project/Program.cs
+++ SODV - Connect 4 Final Project/Program.cs
@@ -4,9 +4,42 @@ namespace SODV___Connect_4_Final_Project
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void (Main(string[] args)
+            {
+            Game game = new Game();
+        game.Start();
+
         }
+    }
+
+class Game
+
+{
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private bool isGamefinished;
+
+    public Game()
+    {
+        board = new Board();
+        player1 = new Player('X', "Player 1");
+        Player2 = new Player('O', "Computer");
+        currentPlayerPlayer = player1;
+        isGamefinished = false;
+    }
+
+}
+
+class Player
+{
+    public char Symbol { get; }
+    public string Name { get; }
+
+    public Player (char symbol, string name)
+    {
+        Symbol= symbol;
+        Name= name;
     }
 }
