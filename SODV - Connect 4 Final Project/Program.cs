213973d035ceb570944f0cabcc1dce538cc07863
Friedel <f.navarro417@mybvc.ca>
2023-06-18 08:59:03 -0600

    Define Interfaces and Basic Classes - Need to revise the code to incorporate Interface as per our Professros instruction

--- SODV - Connect 4 Final Project/Program.cs
+++ SODV - Connect 4 Final Project/Program.cs
@@ -2,6 +2,69 @@
 
 namespace SODV___Connect_4_Final_Project
 {
+        interface IPlayer
+    {
+        char Symbol { get; } // Represents the player's symbol (X or O)
+        string Name { get; } // Represents the player's name
+        int GetMove(Board board); // Get the player's move
+    }
+
+    class HumanPlayer : IPlayer
+    {
+        public char Symbol { get; }
+        public string Name { get; }
+
+        public HumanPlayer(char symbol, string name)
+        {
+            Symbol = symbol;
+            Name = name;
+        }
+
+        public int GetMove(Board board)
+        {
+            int column = 0;
+            bool isValidMove = false;
+
+            while (!isValidMove)
+            {
+                string input = Console.ReadLine();
+                isValidMove = int.TryParse(input, out column) && board.IsColumnValid(column);
+
+                if (!isValidMove)
+                {
+                    Console.WriteLine("Invalid input. Please select a valid column (1-7).");
+                }
+            }
+
+            return column;
+        }
+    }
+
+    class ComputerPlayer : IPlayer
+    {
+        public char Symbol { get; }
+        public string Name { get; }
+
+        public ComputerPlayer(char symbol, string name)
+        {
+            Symbol = symbol;
+            Name = name;
+        }
+
+        public int GetMove(Board board)
+        {
+            Random random = new Random();
+            int column;
+
+            do
+            {
+                column = random.Next(1, 8);
+            } while (!board.IsColumnValid(column));
+
+            return column;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -11,72 +74,7 @@ namespace SODV___Connect_4_Final_Project
         }
     }
 
-    class Game
-    {
-        private Board board; // Represents the game board
-        private Player player1; // Represents player 1
-        private Player player2; // Represents player 2 or the computer
-        private Player currentPlayer; // Represents the current player
-        private bool isGameFinished; // Indicates if the game is finished
 
-        public Game()
-        {
-            board = new Board();
-            player1 = new Player('X', "Player 1");
-            player2 = new Player('O', "Player 2");
-            currentPlayer = player1;
-            isGameFinished = false;
-        }
 
-        public void Start()
-        {
-            Console.WriteLine("Welcome to Connect Four!");
-            Console.WriteLine("Please select a mode:");
-            Console.WriteLine("1. 2-Player Mode");
-            Console.WriteLine("2. 1-Player Mode (Player vs Computer)");
-            Console.WriteLine("3. Exit");
 
-            char mode = Console.ReadKey().KeyChar; // Read the selected mode from the user
-            Console.WriteLine();
-
-            switch (mode)
-            {
-                case '1':   
-                    TwoPlayerMode(); // Start the 2-player mode
-                    break;
-                case '2':
-                    SinglePlayerMode(); // Start the 1-player mode
-                    break;
-                case '3':
-                    Environment.Exit(0); // Exit the application
-                    break;
-                default:
-                    Console.WriteLine("Invalid input. Please try again.\n");
-                    Start(); // Prompt the user to select a valid mode
-                    break;
-            }
-        }
-    }
-
-    class Board
-    {
-        private char[,] grid; // Represents the game board grid
-
-        public Board()
-        {
-            grid = new char[6, 7]; // Initialize the game board grid with a size of 6x7
-        }
-    }
-
-    class Player
-    {
-        public char Symbol { get; } // Represents the player's symbol (X or O)
-        public string Name { get; } // Represents the player's name
-
-        public Player(char symbol, string name)
-        {
-            Symbol = symbol;
-            Name = name;
-        }
-    }
 }
