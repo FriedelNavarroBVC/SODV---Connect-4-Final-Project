03baa20f2a01311df70505f162d7ef2724c152c3
Friedel <f.navarro417@mybvc.ca>
2023-06-19 13:54:08 -0600

    Phase 2: Implement the Game Logic Added Game Class

--- SODV - Connect 4 Final Project/Program.cs
+++ SODV - Connect 4 Final Project/Program.cs
@@ -74,7 +74,52 @@ namespace SODV___Connect_4_Final_Project
         }
     }
 
+    class Game
+    {
+        private Board board; // Represents the game board
+        private IPlayer player1; // Represents player 1
+        private IPlayer player2; // Represents player 2 or the computer
+        private IPlayer currentPlayer; // Represents the current player
+        private bool isGameFinished; // Indicates if the game is finished
+
+        public Game()
+        {
+            board = new Board();
+            player1 = new HumanPlayer('X', "Player 1");
+            player2 = new HumanPlayer('O', "Player 2");
+            currentPlayer = player1;
+            isGameFinished = false;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Connect Four!");
+            Console.WriteLine("Please select a mode:");
+            Console.WriteLine("1. 2-Player Mode");
+            Console.WriteLine("2. 1-Player Mode (Player vs Computer)");
+            Console.WriteLine("3. Exit");
+
+            char mode = Console.ReadKey().KeyChar; // Read the selected mode from the user
+            Console.WriteLine();
+
+            switch (mode)
+            {
+                case '1':
+                    TwoPlayerMode(); // Start the 2-player mode
+                    break;
+                case '2':
+                    SinglePlayerMode(); // Start the 1-player mode
+                    break;
+                case '3':
+                    Environment.Exit(0); // Exit the application
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.\n");
+                    Start(); // Prompt the user to select a valid mode
+                    break;
+            }
+        }
 
 
 
-}
+    }
